6d61d9e21dd1338f0c0ce94b8879c1899e017978
jperezviloria <protobot.dev@gmail.com>
2020-11-10 23:52:34 -0300

    fix a bug, because on index component the ReactDOM was duplicated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-import ReactDOM from 'react-dom'
-=======
 import ReactDOM from 'react-dom';
->>>>>>> c4d84651a7f2212471832bd3a61a55883d7ed17c
 import App from './App';
 
 ReactDOM.render(
